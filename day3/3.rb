0ae7a6a442b32666b32b58d54fe222fafd1139ed
AdityaGattu <gattuaditya@gmail.com>
2020-01-04 19:01:06 +0530

    firsy commit

---
+++ day3/3.rb
@@ -0,0 +1,19 @@
+class A
+  public
+   def m1
+      puts "public method1"
+ end
+
+   def m2
+      puts "public method2"
+ end
+  #public  :m1,:m2
+end 
+
+a1=A.new
+a1.m1
+a1.m2
+
+#by default initalize method is private.
+#for private ouput is an error
+
